1ab126eb239e38d9aeb093676fc45bb1f074f155
awesome8 <awesome8@163.com>
2021-09-04 16:08:04 +0800

    ARA_Category升级

--- ARA_Category/ARA_Category/ARA_Category/CTMediator+A.h
+++ ARA_Category/ARA_Category/ARA_Category/CTMediator+A.h
@@ -6,10 +6,14 @@
 //
 
 #import <CTMediator/CTMediator.h>
+#import <UIKit/UIKit.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface CTMediator (A)
+
+- (UIViewController *)A_Category_Swift_ViewControllerWithCallback:(void(^)(NSString *result))callback;
+- (UIViewController *)A_Category_Objc_ViewControllerWithCallback:(void(^)(NSString *result))callback;
 
 @end
 
--- ARA_Category/ARA_Category/ARA_Category/CTMediator+A.m
+++ ARA_Category/ARA_Category/ARA_Category/CTMediator+A.m
@@ -9,4 +9,19 @@
 
 @implementation CTMediator (A)
 
+- (UIViewController *)A_Category_Swift_ViewControllerWithCallback:(void (^)(NSString *))callback
+{
+    NSMutableDictionary *params = [[NSMutableDictionary alloc] init];
+    params[@"callback"] = callback;
+    params[kCTMediatorParamsKeySwiftTargetModuleName] = @"A_swift";
+    return [self performTarget:@"A" action:@"Category_ViewController" params:params shouldCacheTarget:NO];
+}
+
+- (UIViewController *)A_Category_Objc_ViewControllerWithCallback:(void (^)(NSString *))callback
+{
+    NSMutableDictionary *params = [[NSMutableDictionary alloc] init];
+    params[@"callback"] = callback;
+    return [self performTarget:@"A" action:@"Category_ViewController" params:params shouldCacheTarget:NO];
+}
+
 @end
